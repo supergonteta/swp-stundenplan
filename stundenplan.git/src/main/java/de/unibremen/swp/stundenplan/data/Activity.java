527d1fcba2431fc8499f4af1f9b4567938497dd4
Roman <Roman@134.102.113.5>
2014-08-04 13:47:50 +0200

    Aktivität erstellt

---
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/data/Activity.java
@@ -0,0 +1,12 @@
+package de.unibremen.swp.stundenplan.data;
+
+import java.io.Serializable;
+
+public class Activity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2597139574206115534L;
+
+}
