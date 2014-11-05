b214851edea32df1586c57b833d25635b39cfc19
oliver <oliver@tzi.de>
2014-11-05 13:37:38 +0100

    bildschirmauflösung, farben

--- stundenplan.git/guiprototype/gui/MainFrame.java
+++ stundenplan.git/guiprototype/gui/MainFrame.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.*;
 
@@ -8,22 +10,26 @@ public class MainFrame extends JFrame{
 	
 	public MainFrame() {
 		super("GUI-Prototype");
-		setSize(600, 600);
+		
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		
+		setSize(screen.width, screen.height);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panelRot = new JPanel();
-        JPanel panelBlue = new JPanel();
-        JPanel panelGreen = new JPanel();
+		JPanel panelDaten = new JPanel();
+        JPanel panelStundenplan = new JPanel();
+        JPanel panelLehrer = new JPanel();
         
-        panelRot.setBackground(Color.RED);
-        panelBlue.setBackground(Color.BLUE);
-        panelGreen.setBackground(Color.GREEN);
+        panelDaten.setBackground(Color.LIGHT_GRAY);
+        panelStundenplan.setBackground(Color.LIGHT_GRAY);
+        panelLehrer.setBackground(Color.LIGHT_GRAY);
 		
 		JTabbedPane tabpane = new JTabbedPane(JTabbedPane.TOP);
 		
-		tabpane.addTab("Ich bin rot", panelRot);
-        tabpane.addTab("Ich bin blau", panelBlue);
-        tabpane.addTab("Ich bin gr√ºn", panelGreen);
+		tabpane.addTab("Daten", panelDaten);
+        tabpane.addTab("Stundenpl‰ne", panelStundenplan);
+        tabpane.addTab("Lehreransicht", panelLehrer);
 		
         add(tabpane);
         
